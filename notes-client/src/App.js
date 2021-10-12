8d64a42c79b1419bbd67084e473bd3fe84e8f89a
TurboGor <hayden.chan@students.mq.edu.au>
2021-10-12 13:02:12 +1100

    Added template and note component

--- notes-client/src/App.js
+++ notes-client/src/App.js
@@ -1,25 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import Note from './components/Note';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = { notes: [], filter: 'none' }
+  async componentDidMount() {
+    try {
+      const { data: { listNotes: { items }}} = null //await API call
+      this.setState({ notes: items })
+    } catch (err) {
+      console.log('error fetching notes...', err)
+    }
+  }
+  createNote = async note => {
+    const notes = [note, ...this.state.notes]
+    const newNotes = this.state.notes
+    this.setState({ notes })
+    try {
+      const data = null //await API call
+      this.setState({ notes: [data.data.createNote, ...newNotes] })
+    } catch (err) {
+      console.log('error creating note..', err)
+    }
+  }
+  updateNote = async note => {
+    const updatedNote = {
+      ...note,
+      status: note.status === 'new' ? 'completed' : 'new'
+    }
+    const index = this.state.notes.findIndex(i => i.id === note.id)
+    const notes = [...this.state.notes]
+    notes[index] = updatedNote
+    this.setState({ notes })
+
+    try {
+      //await API call
+    } catch (err) {
+      console.log('error updating note...', err)
+    }
+  }
+  deleteNote = async note => {
+    const input = { id: note.id }
+    const notes = this.state.notes.filter(n => n.id !== note.id)
+    this.setState({ notes })
+    try {
+      //await API call
+    } catch (err) {
+      console.log('error deleting note...', err)
+    }
+  }
+  updateFilter = filter => this.setState({ filter })
+  render() {
+    let { notes, filter } = this.state
+    if (filter === 'completed') {
+      notes = notes.filter(n => n.status === 'completed')
+    }
+    if (filter === 'new') {
+      notes = notes.filter(n => n.status === 'new')
+    }
+    return (
+      <div>
+        <p>Notes</p>
+        <div>
+          <p
+            onClick={() => this.updateFilter('none')}
+          >All</p>
+          <p
+            onClick={() => this.updateFilter('completed')}
+          >Completed</p>
+          <p
+            onClick={() => this.updateFilter('new')}
+          >Pending</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
