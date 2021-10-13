548e6df563c49a2e3978572a430ca0e427f400b0
Lindsay <sperringlindsay@gmail.com>
2021-10-14 09:26:46 +1100

    revert

--- notes-client/src/App.js
+++ notes-client/src/App.js
@@ -18,18 +18,6 @@ class App extends Component {
     } catch (err) {
       console.log("error fetching notes...", err);
     }
-
-    const newSocket = io(`http://localhost:5000`);
-    console.log(newSocket);
-    this.setState({ socket: newSocket });
-    const editor = this.quillEditor.current.getEditor();
-    newSocket.on("uuid", (uuid) => {
-      this.setState({ uuid });
-    });
-    newSocket.on("update", (update) => {
-      console.log(update);
-      editor.updateContents(update.delta);
-    });
   }
   componentWillUnmount() {
     this.state.socket.close();
@@ -88,16 +76,6 @@ class App extends Component {
           <p onClick={() => this.updateFilter("completed")}>Completed</p>
           <p onClick={() => this.updateFilter("new")}>Pending</p>
         </div>
-        <ReactQuill
-          onChange={(html, delta, source, editor) => {
-            if (source !== "user") return;
-            this.state.socket.emit("change", delta);
-            // this.setState({ quill: changes });
-          }}
-          ref={this.quillEditor}
-          modules={this.modules}
-          formats={this.formats}
-        />
         <button
           onClick={() => {
             this.setState({ quill: "<p>Test</p>" });
