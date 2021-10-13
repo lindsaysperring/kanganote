dec6d3b7f1e7d35e913c7cc139de8bd7de3f1440
Lindsay <sperringlindsay@gmail.com>
2021-10-13 11:52:56 +1100

    working sockets and updates

--- notes-server/server.js
+++ notes-server/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+
 const app = express();
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(express.json());
 
@@ -32,6 +40,7 @@ const options = {
 
 const specs = swaggerJsdoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+const users = new Map();
 
 mongoose.connect(
   "mongodb+srv://comp3120_group:xFywsSoJkHfUEuVQ@cluster0.gyscn.mongodb.net/comp3120_notes?retryWrites=true&w=majority",
@@ -43,6 +52,20 @@ mongoose.connect(
 
 io.on("connection", (socket) => {
   console.log("user connected");
+  users.set(socket, {
+    id: uuidv4(),
+  });
+  socket.emit("uuid", users.get(socket).id);
+  socket.on("change", (message) => {
+    io.sockets.emit("update", {
+      clientId: users.get(socket).id,
+      delta: message,
+    });
+    console.log(message);
+  });
+  socket.on("disconnect", () => {
+    users.delete(socket);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
