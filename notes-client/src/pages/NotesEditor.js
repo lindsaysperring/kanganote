8706fc5deb94e4cde073111572881dea3773c86b
Lindsay <sperringlindsay@gmail.com>
2021-10-14 09:32:19 +1100

    editor and sockets

--- notes-client/src/pages/NotesEditor.js
+++ notes-client/src/pages/NotesEditor.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import "react-quill/dist/quill.snow.css";
+
+import React, { useEffect, useState } from "react";
+
+import ReactQuill from "react-quill";
+import io from "socket.io";
 
 export default function NotesEditor() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [socket, setSocket] = useState(null);
+  useEffect(() => {
+    const newSocket = io(`http://localhost:5000`);
+    setSocket(newSocket);
+    const editor = this.quillEditor.current.getEditor();
+    newSocket.on("update", (update) => {
+      console.log(update);
+      editor.updateContents(update.delta);
+    });
+    return () => {
+      newSocket.close();
+    };
+  }, []);
+  return (
+    <div>
+      <ReactQuill
+        onChange={(html, delta, source, editor) => {
+          if (source !== "user") return;
+          socket.emit("change", delta);
+        }}
+        ref={this.quillEditor}
+        modules={this.modules}
+        formats={this.formats}
+      />
+    </div>
+  );
 }
