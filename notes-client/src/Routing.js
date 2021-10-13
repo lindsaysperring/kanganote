c6d4a2378a05242355c58ad003d57a05227106bd
Lindsay <sperringlindsay@gmail.com>
2021-10-14 09:11:31 +1100

    added login and register requests, and context for user data

--- notes-client/src/Routing.js
+++ notes-client/src/Routing.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React, { useState } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import Signup from './pages/LandingPage'
-import Login from './pages/LoginPage'
+import Home from "./pages/Home";
+import Login from "./pages/LoginPage";
+import Signup from "./pages/LandingPage";
 
+export const userContext = React.createContext({
+  userData: { isLoggedIn: false, user: null },
+  setUserData: () => {},
+});
 
 const Routing = () => {
-  return (
-    <Router>
-          <Switch>
-            <Route exact path="/" component={Signup} />
-            <Route path="/login" component={Login} />
-          </Switch>
-    </Router>
-  )
-}
+  const [userData, setUserData] = useState({ isLoggedIn: false, user: null });
+  const value = { userData, setUserData };
 
-export default Routing
+  return (
+    <userContext.Provider value={value}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/home" component={Home} />
+        </Switch>
+      </Router>
+    </userContext.Provider>
+  );
+};
+
+export default Routing;
