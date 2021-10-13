4928072aede0329b6dba5354b977ab387a18775a
Lindsay <sperringlindsay@gmail.com>
2021-10-13 11:52:56 +1100

    working sockets and updates

--- notes-client/src/App.js
+++ notes-client/src/App.js
@@ -18,6 +18,22 @@ class App extends Component {
     } catch (err) {
       console.log("error fetching notes...", err);
     }
+
+    const newSocket = io(`http://localhost:5000`);
+    console.log(newSocket);
+    this.setState({ socket: newSocket });
+    const editor = this.quillEditor.current.getEditor();
+    newSocket.on("uuid", (uuid) => {
+      this.setState({ uuid });
+    });
+    newSocket.on("update", (update) => {
+      console.log(update)
+      if (update.clientId === this.state.uuid) return;
+      editor.updateContents(update.delta);
+    });
+  }
+  componentWillUnmount() {
+    this.state.socket.close();
   }
   createNote = async (note) => {
     const notes = [note, ...this.state.notes];
@@ -73,6 +89,21 @@ class App extends Component {
           <p onClick={() => this.updateFilter("completed")}>Completed</p>
           <p onClick={() => this.updateFilter("new")}>Pending</p>
         </div>
+        <ReactQuill
+          onChange={(html, delta, source, editor) => {
+            if (source !== "user") return;
+            this.state.socket.emit("change", delta);
+            // this.setState({ quill: changes });
+          }}
+          ref={this.quillEditor}
+        />
+        <button
+          onClick={() => {
+            this.setState({ quill: "<p>Test</p>" });
+          }}
+        >
+          Hello
+        </button>
       </div>
     );
   }
