b7095bd54edb1787304427c4437eb5172954ce8e
Lindsay <sperringlindsay@gmail.com>
2021-10-14 09:37:18 +1100

    change from class to function

--- notes-client/src/pages/NotesEditor.js
+++ notes-client/src/pages/NotesEditor.js
@@ -1,16 +1,18 @@
 import "react-quill/dist/quill.snow.css";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import ReactQuill from "react-quill";
-import io from "socket.io";
+import io from "socket.io-client";
 
 export default function NotesEditor() {
   const [socket, setSocket] = useState(null);
+  const quillEditor = useRef(null);
+
   useEffect(() => {
     const newSocket = io(`http://localhost:5000`);
     setSocket(newSocket);
-    const editor = this.quillEditor.current.getEditor();
+    const editor = quillEditor.current.getEditor();
     newSocket.on("update", (update) => {
       console.log(update);
       editor.updateContents(update.delta);
@@ -19,6 +21,36 @@ export default function NotesEditor() {
       newSocket.close();
     };
   }, []);
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
   return (
     <div>
       <ReactQuill
@@ -26,9 +58,9 @@ export default function NotesEditor() {
           if (source !== "user") return;
           socket.emit("change", delta);
         }}
-        ref={this.quillEditor}
-        modules={this.modules}
-        formats={this.formats}
+        ref={quillEditor}
+        modules={modules}
+        formats={formats}
       />
     </div>
   );
