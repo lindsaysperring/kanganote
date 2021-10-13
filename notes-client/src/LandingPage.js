82cf2393e6afa27b082c9c025ccfed44ed952212
JY2D <monhun4u@gmail.com>
2021-10-13 14:36:42 +1100

    added sign up form on landing Page

--- notes-client/src/LandingPage.js
+++ notes-client/src/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Formik, Field, Form } from "formik";
 
 import './LandingPage.css'
 
@@ -14,6 +15,76 @@ const LandingPage = () => {
       </div>
       <div className="form">
         <h1>This is the sign up form</h1>
+        <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+            password:'',
+          }}
+          onSubmit={({ setSubmitting }) => {
+            alert("Form is validated!")
+            setSubmitting(false)
+          }}
+          >
+            {({ touched, errors, isSubmitting }) => (
+              <Form>
+                <div className="form">
+                <label htmlFor="firstName">First Name</label>
+                <Field
+                  type="text"
+                  name="firstName"
+                  placeholder="Enter Your First Name"
+                />
+                {errors.firstName && touched.firstName && (
+                  <p>{errors.firstName}</p>
+                )}
+                </div>
+
+                <div className="form">
+                <label htmlFor="lastName">Last Name</label>
+                <Field
+                  type="text"
+                  name="lastName"
+                  placeholder="Enter Your Last Name"
+                />
+                {errors.lastName && touched.lastName && (
+                  <p>{errors.lastName}</p>
+                )}
+                </div>
+
+                <div className="form">
+                <label htmlFor="email">Email</label>
+                <Field
+                  type="text"
+                  name="email"
+                  placeholder="Enter Your Email"
+                />
+                {errors.email && touched.email && (
+                  <p>{errors.email}</p>
+                )}
+                </div>
+
+                <div className="form">
+                <label htmlFor="password">Password</label>
+                <Field
+                  type="password"
+                  name="password"
+                  placeholder="Enter Your Password"
+                />
+                {errors.password && touched.password && (
+                  <p>{errors.password}</p>
+                )}
+                </div>
+                <div className="button">
+                  <button type="submit">
+                    Sign Up
+                  </button>
+                </div>
+              </Form>
+            )}
+
+          </Formik>
       </div>
       </div>
     </div>
