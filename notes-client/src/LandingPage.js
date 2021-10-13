a24aff4d37bd170c09e71f0fc539e4f4b521073c
JY2D <monhun4u@gmail.com>
2021-10-13 14:28:39 +1100

    Making Landing Page template

---
+++ notes-client/src/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './LandingPage.css'
+
+const LandingPage = () => {
+
+  return (
+    <div className="backContainer">
+      <div className="columns">
+      <div className="info">
+        <h1> This is our Note taking website TITLE </h1>
+        <h3> intro text</h3>
+        <h3> intro img</h3>
+      </div>
+      <div className="form">
+        <h1>This is the sign up form</h1>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
+
