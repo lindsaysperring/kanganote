64a922dad343829c7294b267e89720e0456167f9
JY2D <monhun4u@gmail.com>
2021-10-28 13:35:53 +1100

    Sidemenu fix-up

--- notes-client/src/pages/Menu/SideMenu.js
+++ notes-client/src/pages/Menu/SideMenu.js
@@ -1,26 +1,45 @@
-import React, {useContext, useState} from 'react';
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
-import { userContext } from "../../Routing";
+import { SidebarData } from './SideMenuData';
 
 import './SideMenu.css'
 
-const SideMenu = (props) => {
-  const { userData } = useContext(userContext);
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
 
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
-    <div className='start' style={{width: props.width}}>
-          {userData.isLoggedIn && (
-            <>
-      <h1> Welcome {userData.user.name} </h1>
-      <a href= '/profile'>Profile</a>
-      <a href='/completed'>Completed Notes</a>
-      <a href='/settings'>Settings</a>
-      <a href='/'>Log Out</a>
-      </>
-      )}
-    </div>
-  )
+    <>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+    </>
+  );
 }
 
-export default SideMenu;
+export default Navbar;
---
+++ notes-client/src/pages/Menu/SideMenuData.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as MdIcons from 'react-icons/md';
+import * as IoIcons from 'react-icons/io';
+import * as CgIcons from "react-icons/cg";
+
+export const SidebarData = [
+  {
+    title: 'Profile',
+    path: '/profile',
+    icon: <CgIcons.CgProfile />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Completed',
+    path: '/completed',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Settings',
+    path: '/settings',
+    icon: <MdIcons.MdOutlineSettings />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Logout',
+    path: '/',
+    icon: <MdIcons.MdPowerSettingsNew />,
+    cName: 'nav-text'
+  },
+
+];
